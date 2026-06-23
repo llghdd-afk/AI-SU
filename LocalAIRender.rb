ea6b3464b107200e7110af1e5b765065f3128447
llghdd-afk <llghdd@gmail.com>
2026-06-23 08:36:32 +0800

    Improve inspiration result browsing layout

--- LocalAIRender.rb
+++ LocalAIRender.rb
@@ -6,7 +6,7 @@ require 'extensions.rb'
 module LLGHD
   module LocalAIRender
     EXTENSION_NAME = 'SU Local AI Render'
-    EXTENSION_VERSION = '0.4.29'
+    EXTENSION_VERSION = '0.4.30'
     EXTENSION_ID = 'llghd.local_ai_render'
 
     loader = File.join(__dir__, 'LocalAIRender', 'core.rb')
